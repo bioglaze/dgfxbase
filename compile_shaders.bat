bc1470fbadc33ca30f687af8b257d941a45a0ea0
Timo Wiren <timo.wiren@gmail.com>
2018-10-12 18:50:51 +0300

    SDK location from an environment variable

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,3 +1,3 @@
-\VulkanSDK\1.0.65.0\Bin\glslangValidator.exe -V assets\line_shader.frag -o assets\line_shader.frag.spv
-\VulkanSDK\1.0.65.0\Bin\glslangValidator.exe -V assets\line_shader.vert -o assets\line_shader.vert.spv
+%VULKAN_SDK%\Bin\glslangValidator.exe -V assets\line_shader.frag -o assets\line_shader.frag.spv
+%VULKAN_SDK%\Bin\glslangValidator.exe -V assets\line_shader.vert -o assets\line_shader.vert.spv
 pause
